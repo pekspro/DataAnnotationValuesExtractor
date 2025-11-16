ece610f34d22336840671041a256c8ef050851d6
pekspro <msn@pekspro.com>
2025-11-16 19:54:16 +0100

    Changes version to 1.0.0.

--- Source/Library/Pekspro.DataAnnotationValuesExtractor/Constants.cs
+++ Source/Library/Pekspro.DataAnnotationValuesExtractor/Constants.cs
@@ -3,6 +3,5 @@ namespace Pekspro.DataAnnotationValuesExtractor;
 public static class Constants
 {
     // Note. Should also be changed in Version.props.
-    public const string Version = "0.0.1";
+    public const string Version = "1.0.0";
 }
-
